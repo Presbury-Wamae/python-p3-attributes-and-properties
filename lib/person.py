f2afd3f6f3fe1e0b496fd1f936a268ffc8d6058f
Presbury-Wamae <wamaepresbury034@gmail.com>
2025-05-13 14:46:52 +0300

    .

--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,37 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name=None, job=None):
+        self._name = None
+        self._job = None
+        if name is not None:
+            self.name = name
+        if job is not None:
+            self.job = job
+    
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and 1 <= len(name) <= 25:
+            self._name = name.title()
+        else:
+            self._name = None
+            print("Name must be string between 1 and 25 characters.")
+    
+    @property
+    def job(self):
+        return self._job
+
+    @job.setter
+    def job(self, job):
+        if job in APPROVED_JOBS:
+            self._job = job
+        else:
+            self._job = None
+            print("Job must be in list of approved jobs.")    
+        
+    
+        
